654f18f9f2e881c466fc3bf273aba1edbd36c3dd
informaticacomense <g.corti@informaticacomense.it>
2025-10-17 20:17:29 +0200

    Create prisma.ts

---
+++ prisma.ts
@@ -0,0 +1,17 @@
+// src/lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+// Evita di creare più istanze in ambiente dev/hot reload
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
