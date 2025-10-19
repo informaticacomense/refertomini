bd2f321b05d761c72080fa7fd068a95ac2b93446
informaticacomense <g.corti@informaticacomense.it>
2025-10-19 18:25:46 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,8 @@
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: { extend: {} },
+  plugins: []
 };
